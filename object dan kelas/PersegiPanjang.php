042efab304364c3701c28b32a5b2b4fab67f85cc
imamudin90 <115957425+imamudin90@users.noreply.github.com>
2022-12-24 23:44:58 +0700

    Add files via upload

---
+++ object dan kelas/PersegiPanjang.php
@@ -0,0 +1,26 @@
+<?php
+class PersegiPanjang {
+    public $panjang;
+    public $lebar;
+
+    public function __construct($panjang, $lebar) {
+        $this->panjang=($panjang);
+        $this->lebar=($lebar);
+    }
+    public function panjang() {
+        return $this->panjang ;
+    }
+    public function lebar() {
+        return $this->lebar ;
+    }
+    public function hitungLuas() {
+        return $this->panjang * $this->lebar;
+    }
+}
+
+$persegiPanjang = new PersegiPanjang(20,3);
+echo "Panjangnya adalah =" .$persegiPanjang->panjang();
+echo "<br>";
+echo "Lebarnya adalah = " .$persegiPanjang->panjang();
+echo "<br>";
+echo "Luas = ".$persegiPanjang->hitungLuas();
---
+++ object dan kelas/Segitiga.php
@@ -0,0 +1,26 @@
+<?php
+class Segitiga{
+    public $alas;
+    public $tinggi;
+
+    public function __construct($alas, $tinggi){
+        $this->alas=($alas);
+        $this->tinggi=($tinggi);
+    }
+    public function alas() {
+        return $this->alas ;
+    }
+    public function tinggi() {
+        return $this->tinggi ;
+    }
+    public function hitungLuas() {
+        return $this->alas * 1/2 * $this->tinggi;
+    }
+}
+
+$luasSegitiga = new Segitiga(20,3);
+echo "alas :".$luasSegitiga->alas();
+echo "<br>";
+echo "tinggi :".$luasSegitiga->tinggi();
+echo "<br>";
+echo "maka luas segitiga =".$luasSegitiga->hitungLuas();
